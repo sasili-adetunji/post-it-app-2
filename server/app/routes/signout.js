b41d5da0e6c7cee26a0403c5a6a9faa9e57ea107
unknown <shazily012@gmail.com>
2017-06-01 03:39:53 -0700

    updates

--- server/app/routes/signout.js
+++ server/app/routes/signout.js
@@ -13,31 +13,13 @@ const signout = (app, db) => {
   firebase.auth().signOut()
   .then(() => {
             // redirect to home page after signout
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 5a5222c40ad3aa257050eefb27abcf15c6be3595
-    res.send({
+   res.send({
       message: 'You have signed out f the Appliction'
     })
-  })
-  .catch((err) => {
-    res.json({
-      message: 'There appear to be ' + err.message + ' with signing out'
-<<<<<<< HEAD
-=======
-  	res.send({
-  		message: 'You have signed out f the Appliction'
-  	})
-  })
+  })    
   .catch((err) => {
     res.json({
     	message: 'There appear to be ' + err.message + ' with signing out'
->>>>>>> fb950ca865531320c85b57abb71633b9d726a2e4
-=======
-
->>>>>>> 5a5222c40ad3aa257050eefb27abcf15c6be3595
     });
   })
 
--- server/config/db.js
+++ server/config/db.js
@@ -11,23 +11,7 @@ const config = {
 };
 
 let db = firebase.initializeApp(config);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 5a5222c40ad3aa257050eefb27abcf15c6be3595
 
 export const ref = firebase.database().ref()
 export const firebaseAuth = firebase.auth
 export default db;
-
-
-<<<<<<< HEAD
-=======
-export const firebaseAuth = firebase.auth;
-export const ref = firebase.database().ref();
-export default db;
->>>>>>> fb950ca865531320c85b57abb71633b9d726a2e4
-=======
-
->>>>>>> 5a5222c40ad3aa257050eefb27abcf15c6be3595
