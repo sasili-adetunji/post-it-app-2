387489910a4c08d75d34a3376761304cc03b569c
unknown <shazily012@gmail.com>
2017-06-23 04:09:58 -0700

    update ile

--- server/app/routes/userGroup.js
+++ server/app/routes/userGroup.js
@@ -10,7 +10,7 @@ import { ref, firebaseAuth } from '../../config/db'
 const userGroup = (app, db) => {
 	firebaseAuth().onAuthStateChanged((user) => {
 
-	app.get('/user/message', (req, res) => {
+	app.get('/user/groups', (req, res) => {
         const userRef = fb.ref(`users/${user.uid}/groups/groupInfo`)
         .once('child_added', msg =>{
         	const data = msg.val()
