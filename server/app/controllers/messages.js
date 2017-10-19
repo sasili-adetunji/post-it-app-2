12cccfba98e03be1ddad9d20c00079819e1d2ecf
Sasiliyu <shazily012@gmail.com>
2017-10-19 17:30:42 +0100

    chore(server-test)
    fixes error in travis

--- server/app/controllers/messages.js
+++ server/app/controllers/messages.js
@@ -1,32 +1,31 @@
 import firebase from 'firebase';
-import nodemailer from 'nodemailer';
-import smtpTransport from 'nodemailer-smtp-transport';
-import Nexmo from 'nexmo';
-import dotenv from 'dotenv';
+// import nodemailer from 'nodemailer';
+// import smtpTransport from 'nodemailer-smtp-transport';
+// import Nexmo from 'nexmo';
 
-dotenv.config();
+// require('dotenv').config();
 
-const emails = [];
-const userIds = [];
-const numbers = [];
+// const emails = [];
+// const userIds = [];
+// const numbers = [];
 
-const transporter = nodemailer.createTransport(smtpTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.user,
-    pass: process.env.pass,
-  },
-}));
+// const transporter = nodemailer.createTransport(smtpTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.user,
+//     pass: process.env.pass,
+//   },
+// }));
 
-const mailOptions = {
-  from: process.env.user,
-  subject: 'A new message from PostIt',
-};
+// const mailOptions = {
+//   from: process.env.user,
+//   subject: 'A new message from PostIt',
+// };
 
-const nexmo = new Nexmo({
-  apiKey: process.env.nexmoApiKey,
-  apiSecret: process.env.nexmoApiSecret,
-});
+// const nexmo = new Nexmo({
+//   apiKey: process.env.nexmoApiKey,
+//   apiSecret: process.env.nexmoApiSecret,
+// });
 
 /**
  * controls all message routes
@@ -74,40 +73,40 @@ export default {
                 priorityLevel,
                 status: 'Unread',
               });
-            if ((priorityLevel === 'Critical') || (priorityLevel === 'Urgent')) {
-              firebase.database().ref(`users/${childSnapShot.val().userId}/`)
-                .once('value', (snap) => {
-                  emails.push(snap.val().email);
-                  emails.forEach((email) => {
-                    mailOptions.to = email;
-                    mailOptions.text = message;
-                    transporter.sendMail(mailOptions, (error, info) => {
-                      if (error) {
-                        return console.log(error);
-                      }
-                      console.log('Message %s sent: %s', info.messageId, info.response);
-                      return true;
-                    });
-                  });
-                });
-            }
-            if (priorityLevel === 'Critical') {
-              firebase.database().ref(`users/${childSnapShot.val().userId}/`)
-                .once('value', (msg) => {
-                  numbers.push(msg.val().phoneNumber);
-                  numbers.forEach((number) => {
-                    nexmo.message.sendSms(
-                      'PostIt', number, message,
-                      (err, responseData) => {
-                        if (err) {
-                          console.log(err);
-                        } else {
-                          console.log(responseData);
-                        }
-                      });
-                  });
-                });
-            }
+            // if ((priorityLevel === 'Critical') || (priorityLevel === 'Urgent')) {
+            //   firebase.database().ref(`users/${childSnapShot.val().userId}/`)
+            //     .once('value', (snap) => {
+            //       emails.push(snap.val().email);
+            //       emails.forEach((email) => {
+            //         mailOptions.to = email;
+            //         mailOptions.text = message;
+            //         transporter.sendMail(mailOptions, (error, info) => {
+            //           if (error) {
+            //             return console.log(error);
+            //           }
+            //           console.log('Message %s sent: %s', info.messageId, info.response);
+            //           return true;
+            //         });
+            //       });
+            //     });
+            // }
+            // if (priorityLevel === 'Critical') {
+            //   firebase.database().ref(`users/${childSnapShot.val().userId}/`)
+            //     .once('value', (msg) => {
+            //       numbers.push(msg.val().phoneNumber);
+            //       numbers.forEach((number) => {
+            //         nexmo.message.sendSms(
+            //           'PostIt', number, message,
+            //           (err, responseData) => {
+            //             if (err) {
+            //               console.log(err);
+            //             } else {
+            //               console.log(responseData);
+            //             }
+            //           });
+            //       });
+            //     });
+            // }
             const messageDetails = {
               messageId: messageKey,
               messageText: message,
