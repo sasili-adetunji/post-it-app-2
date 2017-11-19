ab884684a319a790e6d7a4ebb63c7f4993d8022a
Sasiliyu <shazily012@gmail.com>
2017-11-19 20:07:27 +0100

    chore(jsdoc)
    add jsdoc to store

--- client/stores/PostItStore.js
+++ client/stores/PostItStore.js
@@ -22,120 +22,409 @@ let searchedUsers = '';
 
 const PostItStore = assign({}, EventEmitter.prototype, {
 
+  /**
+ * @description describes a function that set searched users
+ *
+ * @param { String } user
+ *
+ * @method setSearchedUsers
+ *
+ * @returns { void } void
+ */
+
   setSearchedUsers(user) {
     searchedUsers = user;
   },
+
+ /**
+ * @description describes a function that return searched users
+ *
+ * @method getSearchedUsers
+ *
+ * @returns { String } searched users
+ */
   getSearchedUsers() {
     return searchedUsers;
   },
+
+ /**
+ * @description describes a function that cler the store of searched users
+ *
+ * @method clearSearchedUsers
+ *
+ * @returns { void }
+ */
   clearSearchedUsers() {
     searchedUsers = '';
   },
+
+ /**
+ * @description describes a function that add user to group
+ *
+ * @method addUserToGroup
+ *
+ * @param { String } user
+ *
+ * @returns { Array } the users in a group
+ */
   addUserToGroup(user) {
     usersInGroup.push(user);
   },
 
+/**
+ * @description describes a function that signout user
+ *
+ * @method signOutUser
+ *
+ * @returns { void }
+ */
   signOutUser() {
     loggedInUser.length = 0;
     isAuthenticated = false;
   },
 
+
+/**
+ * @description describes a function that set a user as authenticated
+ *
+ * @method setIsAuthenticated
+ *
+ * @param { Boolean }
+ *
+ * @returns { Boolean } returns true;
+ */
   setIsAuthenticated(value) {
     isAuthenticated = value;
   },
 
+/**
+ * @description describes a function that recieves error
+ *
+ * @method receiveErrors
+ *
+ * @param { String } error
+ *
+ * @returns { String } the error
+ */
   receiveErrors(error) {
     errors = error;
   },
 
+/**
+ * @description describes a function that recieve success message
+ *
+ * @method receiveSuccess
+ *
+ * @param { String } message
+ *
+ * @returns { String } returns success mesage;
+ */
   receiveSuccess(message) {
     success = message;
   },
+
+/**
+ * @description describes a function that recieve login success message
+ *
+ * @method receiveLoginSuccess
+ *
+ * @param { String } message
+ *
+ * @returns { String } returns login success mesage;
+ */
   receiveLoginSuccess(message) {
     loginSuccess = message;
   },
+
+/**
+ * @description describes a function that set error message
+ *
+ * @method setErrors
+ *
+ * @param { String } error
+ *
+ * @returns { String } returns error mesage;
+ */
   setErrors(error) {
     errors = error;
   },
+
+/**
+ * @description describes a function thatreturn error
+ *
+ * @method getErrors
+ *
+ * @returns { String } returns success mesage;
+ */
   getErrors() {
     return errors;
   },
+
+/**
+ * @description describes a function that the selected group
+ *
+ * @method getOpenedGroup
+ *
+ * @returns { Array } returns the selected group
+ */
   getOpenedGroup() {
     return selectedGroup;
   },
 
+/**
+ * @description describes a function that recieves read users
+ *
+ * @method getReadUsers
+ *
+ * @returns { Array } returns the users that have read
+ */
   getReadUsers() {
     return readUsers;
   },
 
+/**
+ * @description describes a function that recieves read users
+ *
+ * @method getLoggedInUser
+ *
+ * @returns { Array } returns the loggedin user
+ */
   getLoggedInUser() {
     return loggedInUser;
   },
 
+/**
+ * @description describes a function that return if a user is authenticated
+ *
+ * @method getIsAuthenticated
+ *
+ * @returns { Boolean }
+ */
   getIsAuthenticated() {
     return isAuthenticated;
   },
 
+/**
+ * @description describes a function that return users in a group
+ *
+ * @method getUsersInGroup
+ *
+ * @returns { Array } users in a particular group
+ */
   getUsersInGroup() {
     return usersInGroup;
   },
+
+/**
+ * @description describes a function that return all users
+ *
+ * @method getUsers
+ *
+ * @returns { Array } users
+ */
   getUsers() {
     return users;
   },
 
+
+/**
+ * @description describes a function that return success message
+ *
+ * @method getSuccess
+ *
+ * @returns { String }
+ */
   getSuccess() {
     return success;
   },
+
+/**
+ * @description describes a function that return login success message
+ *
+ * @method getLoginSuccess
+ *
+ * @returns { String }
+ */
   getLoginSuccess() {
     return loginSuccess;
   },
+
+/**
+ * @description describes a function that add groups
+ *
+ * @method addGroups
+ *
+ * @param { String } groups
+ *
+ * @returns { Array }
+ */
   addGroups(groups) {
     groupsUser.push(groups);
   },
+
+/**
+ * @description describes a function that add message
+ *
+ * @method addMessage
+ *
+ * @param { String } message
+ *
+ * @returns { Array }
+ */
   addMessage(message) {
     userMessages.push(message);
   },
+
+/**
+ * @description describes a function that set messages
+ *
+ * @method setMessages
+ *
+ * @param { String } messages
+ *
+ * @returns { Array }
+ */
   setMessages(messages) {
     userMessages = messages;
   },
+
+/**
+ * @description describes a function that set user groups
+ *
+ * @method setUserGroups
+ *
+ * @param { String } groups
+ *
+ * @returns { Array }
+ */
   setUserGroups(groups) {
     groupsUser = groups;
   },
+
+/**
+ * @description describes a function that return all groups of users
+ *
+ * @method getGroupsUser
+ *
+ * @returns { Array }
+ */
   getGroupsUser() {
     return groupsUser;
   },
 
+/**
+ * @description describes a function that return all messages in a groups
+ *
+ * @method getGroupsMessages
+ *
+ * @returns { Array }
+ */
   getGroupsMessages() {
     return userMessages;
   },
 
+/**
+ * @description describes a function that set users that have read a mesage
+ *
+ * @method setReadUsers
+ *
+ * @param { Object } user
+ *
+ * @returns { Array }
+ */
   setReadUsers(user) {
     readUsers = user;
   },
 
+/**
+ * @description describes a function that set logged in user
+ *
+ * @method setLoggedInUser
+ *
+ * @param { Object } user
+ *
+ * @returns { Array }
+ */
   setLoggedInUser(user) {
     loggedInUser = user;
   },
 
+/**
+ * @description describes a function that set logged in user
+ *
+ * @method setUsers
+ *
+ * @param { Object } user
+ *
+ * @returns { Array }
+ */
   setUsers(user) {
     users = user;
   },
+
+/**
+ * @description describes a function that set logged in user
+ *
+ * @method setUsersInGroup
+ *
+ * @param { Object } user
+ *
+ * @returns { Array }
+ */
   setUsersInGroup(user) {
     usersInGroup = user;
   },
 
+/**
+ * @description describes a function that set opened group
+ *
+ * @method setOpenedGroup
+ *
+ * @param { Object } group
+ *
+ * @returns { Array }
+ */
   setOpenedGroup(group) {
     selectedGroup.pop();
     selectedGroup.push(group);
   },
 
+/**
+ * @description PostItStore emit event change
+ *
+ * @method emitChange
+ *
+ * @returns { void }
+ */
   emitChange() {
     this.emit('change');
   },
 
+
+/**
+ * @description add PostItStore change listener
+ *
+ * @param { Object } callback
+ *
+ * @method addChangeListener
+ *
+ * @returns { void }
+ */
+
   addChangeListener(callback) {
     this.on('change', callback);
   },
+
+
+/**
+ * @description Remove PostItStore change listener
+ *
+ * @param { Object } callback
+ *
+ * @method removeChangeListener
+ *
+ * @returns { void }
+ */
 
   removeChangeListener(callback) {
     this.removeListener('change', callback);
--- mock/localStorage.js
+++ mock/localStorage.js
@@ -1,11 +1,14 @@
 /**
  * LocalStorageMock serve as a mock of the localStorage for testing in Jest
+ *
  * @class LocalStorageMock
  */
 class LocalStorageMock {
     /**
      * @description creates a new instance of this class
+     *
      * @constructor
+     *
      * @memberOf LocalStorageMock
      */
   constructor() {
@@ -15,8 +18,9 @@ class LocalStorageMock {
 
     /**
      * @description clears the store
+     *
      * @method
-     * @memberOf LocalStorageMock
+     *
      * @returns {void}
      */
   clear() {
@@ -25,9 +29,9 @@ class LocalStorageMock {
 
     /**
      * @description returns the value stored on the supplied key
-     * @method
-     * @memberOf LocalStorageMock
+     *
      * @param {string} key The item's key to retrieve from
+     *
      * @returns {void}
      */
   getItem(key) {
@@ -36,10 +40,10 @@ class LocalStorageMock {
 
     /**
      * @description sets the store with the supplied key
-     * @method
-     * @memberOf LocalStorageMock
+     *
      * @param {Object} key The key to store
      * @param {string} value The value to set the key to
+     *
      * @returns {void}
      */
   setItem(key, value) {
@@ -48,9 +52,9 @@ class LocalStorageMock {
 
     /**
      * @description removes the item from the store corresponding to the key
-     * @method
-     * @memberOf LocalStorageMock
+     *
      * @param {Object} key The key to remove
+     *
      * @returns {void}
      */
   removeItem(key) {
